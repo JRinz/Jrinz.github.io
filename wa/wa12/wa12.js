7fdc7fbcc0150da180106fe6e05d82e9531333d1
Josephine Rinzler <jori8768@colorado.edu>
2023-04-17 14:43:23 -0600

    update wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,11 +1,11 @@
 
 //project 1
-let employ = '{ "employees" : [' +
+let employ1 = '{ "employees" : [' +
 '{ "firstName":"Sam" , "department":"Tech", "designation":"Manager", "salary":"40000", "raiseEligible":"true" },' +
 '{ "firstName":"Mary" , "department":"Finance", "designation":"Trainee", "salary":"18500", "raiseEligible":"true" },' +
 '{ "firstName":"Bill" , "department":"HR", "designation":"Executive", "salary":"21200", "raiseEligible":"false" } ]}';
 
-const employees = JSON.parse(employ);
+const employees = JSON.parse(employ1);
 
 console.log("problem 1");
 console.log(employees);
@@ -13,10 +13,11 @@ console.log(employees);
 //project 2
 
 let company = '{ "companyDetails": [' + '{"companyName":"Tech Stars"},' + '{"website":"www.techstars.site"} ]}';
+
 const com = JSON.parse(company);
 
 console.log("problem 2");
-console.log(com);
+console.log(company);
 
 //project 3
 
@@ -26,14 +27,16 @@ let emp2 = '"employees": [' +
 '{"firstname": "Bill", "department": "HR", "designation": "Executive", "salary": "21,200", "raise eligable": "False"}, ' +
 '{"firstname": "Anna", "department": "Tech", "designation": "Executive", "salary": "25,600", "raise eligable": "False"}]}';
 
+const employ2 = JSON.parse(emp2);
+
 console.log ("Problem 3"); 
-console.log (employees2) ;
+console.log (employ2) ;
 
 //project 4
-
-let total = 0;
+let sal = employ2["employees"][0].salary + employ2["employees"][1].salary + employ2["employees"][2].salary + employ2["employees"][3].salary;
 
 console.log("Problem 4");
+console.log(sal); 
 
 //project 5
 console.log("Problem 5");
